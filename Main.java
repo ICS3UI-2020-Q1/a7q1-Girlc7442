c7b839dd8049fdac7b9c8a20e24822067040aa1d
Girlc7442 <girlc7442@wrdsb.ca>
2020-10-16 13:26:15 +0000

    finished need to comment

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
 /**
  *
- * @author 
+ * @author Cole Girling
  */
 public class Main {
+
+  public static void examGrade(int examPercentage){
+   
+   if(examPercentage <= 100 && examPercentage >= 80){
+     System.out.println("A");
+   }else if(examPercentage <= 79 && examPercentage >= 70){
+     System.out.println("B");
+   }else if(examPercentage <= 69 && examPercentage >= 60){
+     System.out.println("C");
+   }else if(examPercentage <= 59 && examPercentage >= 50){
+     System.out.println("D");
+   }else{
+     System.out.println("F");
+    }
+   }
+  
 
   /**
    * The method that is executed when you hit the run button.
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
-  }
+   int examPercentage = 67;
+   examGrade(examPercentage);
+
+ }
 }
